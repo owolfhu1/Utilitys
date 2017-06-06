6a441a4b81823a7a2c2726902905996ae7765648
owolfhu1 <owolfhu1@gmail.com>
2017-06-06 07:18:24 -0400

    refacored
    -added private static String[] months: array of months in order
    -added some methods:  printToday(), intToMonth(), monthToInt(), isDate()
    -refactored age()

--- src/DateUtility.java
+++ src/DateUtility.java
@@ -1,152 +1,87 @@
-import java.util.Calendar;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.Period;
 
 public class DateUtility {
 
-	//returns (Mon)th / year or "invalid date"
-	public static String displayCalendar(int month, int year){
-		String monthString = null;
-		switch(month){
-			case 1 : monthString = "Jan"; break;
-			case 2 : monthString = "Feb"; break;
-			case 3 : monthString = "Mar"; break;
-			case 4 : monthString = "Apr"; break;
-			case 5 : monthString = "May"; break;
-			case 6 : monthString = "Jun"; break;
-			case 7 : monthString = "Jul"; break;
-			case 8 : monthString = "Aug"; break;
-			case 9 : monthString = "Sep"; break;
-			case 10 : monthString = "Oct"; break;
-			case 11 : monthString = "Nov"; break;
-			case 12 : monthString = "Dec"; break;
-		}
-		if (monthString == null) return "invalid Month";
-		return String.format("%s %d", monthString, year);
-	}
-	
-	//takes day, month, year displays (mon)th day year or "invalid date"
-	public static String displayCalendar(int day, int month, int year){
-		String monthString = null;
-		switch(month){
-			case 1 : monthString = "Jan"; break;
-			case 2 : monthString = "Feb"; break;
-			case 3 : monthString = "Mar"; break;
-			case 4 : monthString = "Apr"; break;
-			case 5 : monthString = "May"; break;
-			case 6 : monthString = "Jun"; break;
-			case 7 : monthString = "Jul"; break;
-			case 8 : monthString = "Aug"; break;
-			case 9 : monthString = "Sep"; break;
-			case 10 : monthString = "Oct"; break;
-			case 11 : monthString = "Nov"; break;
-			case 12 : monthString = "Dec"; break;
-		}
-		if (monthString == null || day < 1 || lastDayOfMonth(month, year) < day) return "invalid date";
-		return String.format("%s %d %d", monthString, day, year);
-	}
-	
-	//returns boolean if year enter is or isn't a leap year
-	public static boolean isLeapYear(int year){
-		boolean bLeapYear = false;
-		bLeapYear = (year % 4 == 0);
-		bLeapYear = bLeapYear && (year % 100 != 0);
-		bLeapYear = bLeapYear || (year % 400 == 0);
-		return bLeapYear;
-	}
-	
-	//returns age in years, if invalid date is entered returns -1
-	public static int age (int birthDay, int birthMonth, int birthYear) {
-		Date today;
-		Date fullBirthDay;
-		boolean valid = true;
-		if (birthDay < 1 || birthMonth < 1 || birthMonth > 12) valid = false;
-		switch (birthMonth) {
-			//if month has 31 days
-			case 1: case 3: case 5: case 7: case 8: case 10: case 12:
-				if (birthDay > 31) valid = false;
-			break;
-			//if month has 30 days
-			case 4: case 6: case 9: case 11:
-				if (birthDay > 30) valid = false;
-			break;
-			//if month is feb
-			case 2:
-				//if year is a leap year
-				if (isLeapYear(birthYear)) {
-					if (birthDay > 29) valid = false;
-				}
-				else {
-					if (birthDay > 28) valid = false;
-				}
-			break;
-		}
-		//make Date() object for birth day and today
-		Calendar cal = Calendar.getInstance();
-		cal.set(Calendar.YEAR, birthYear);
-		cal.set(Calendar.MONTH, birthMonth);
-		cal.set(Calendar.DAY_OF_MONTH, birthDay);
-		fullBirthDay = cal.getTime();
-		today = new Date();
-		//compare today and birthday
-		if ((today.getTime() - fullBirthDay.getTime()) < 0) valid = false;
-		long dif = (today.getTime() - fullBirthDay.getTime()) / 1000;
-		long year = 31556952;
-		dif = dif / year;
-		if (valid) return (int)dif;
-		else return (int)-1;
-	}
-	
-	//takes first 3 letters of month, and a year and tells you the last day of the month. 
-	//if invalid month is entered, -1 is returned.
-	public static int lastDayOfMonth(String monthString, int year) {
-		boolean valid = false;
-		int monthNumber = 1000;
-		monthString = monthString.toLowerCase();
-		switch (monthString) {
-			case "jan": case "feb": case "mar": case "apr": case "may": case "jun":
-			case "jul": case "aug": case "sep": case "oct": case "nov": case "dec":
-				valid = true;
-			break;
-		}
-		switch (monthString) {
-			case "jan": monthNumber = 31;break;
-			case "feb": 
-				if (isLeapYear(year)) monthNumber = 29;
-				else monthNumber = 28;
-				break;
-			case "mar":monthNumber = 31; break;
-			case "apr":monthNumber = 30; break;
-			case "may":monthNumber = 31; break;
-			case "jun":monthNumber = 30; break;
-			case "jul":monthNumber = 31; break;
-			case "aug":monthNumber = 31; break;
-			case "sep":monthNumber = 30; break;
-			case "oct":monthNumber = 31; break;
-			case "nov":monthNumber = 30; break;
-			case "dec":monthNumber = 31; break;
-		}
-		if (valid) return monthNumber;
-		else return -1;
-	}
-	
-	//overloaded to take int instead of string month value
-	public static int lastDayOfMonth(int monthInt, int year) {
-		String monthString = "";	
-		switch (monthInt) {
-			case 1:  monthString = "jan"; break;
-			case 2:  monthString = "feb"; break;
-			case 3:  monthString = "mar"; break;
-			case 4:  monthString = "apr"; break;
-			case 5:  monthString = "may"; break;
-			case 6:  monthString = "jun"; break;
-			case 7:  monthString = "jul"; break;
-			case 8:  monthString = "aug"; break;
-			case 9:  monthString = "sep"; break;
-			case 10: monthString = "oct"; break;
-			case 11: monthString = "nov"; break;
-			case 12: monthString = "dec"; break;
-		}
-		return lastDayOfMonth(monthString, year);
-	}
+    private static String[] months = {
+            "jan", "feb", "mar", "apr", "may", "jun",
+            "jul", "aug", "sep", "oct", "nov", "dec"
+    };
+
+    //returns string
+    public static String intToMonth(int m) {
+        if (m > 12 || m < 1) return "invalid";
+        return months[m - 1];
+    }
+
+    //returns -1 if month is invalid
+    public static int monthToInt(String m) {
+        m = m.toLowerCase();
+        for (int i = 1; i > 13; i++) if (months[i - 1].equals(m)) return i;
+        return -1;
+    }
+
+    //returns (Mon)th / year or "invalid date"
+    public static String displayCalendar(int month, int year){
+        if (intToMonth(month).equals("invalid")) return "invalid month";
+        return String.format("%s %d", intToMonth(month), year);
+    }
+
+    //takes day, month, year displays (mon)th day year or "invalid date"
+    public static String displayCalendar(int day, int month, int year){
+        if (intToMonth(month).equals("invalid") || day < 1 || lastDayOfMonth(month, year) < day) return "invalid date";
+        return String.format("%s %d %d", intToMonth(month), day, year);
+    }
+
+    //returns boolean if year enter is or isn't a leap year
+    public static boolean isLeapYear(int year){
+        return ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0));
+    }
+
+    //checks if date is valid
+    public static boolean isDate(int day, int month, int year) {
+        return !(month > 12 || month < 1) && !(day > lastDayOfMonth(month, year) || day < 1);
+    }
+
+    //returns age in years, if invalid date is entered returns -1
+    public static int age (int day, int month, int year) {
+        //check is date is real
+        if (!isDate(day, month, year)) return -1;
+
+        LocalDate today = LocalDate.now();
+        LocalDate birthDay = LocalDate.of(year, month, day);
+        
+	//check if day is in future;
+        if (today.toEpochDay() < birthDay.toEpochDay()) return -1;
+
+        //return birthday.
+        return Period.between(birthDay, today).getYears();
+    }
+
+    //takes first 3 letters of month, and a year and tells you the last day of the month.
+    //if invalid month is entered, -1 is returned.
+    public static int lastDayOfMonth(String month, int year) {
+        month = month.toLowerCase();
+        switch (month) {
+            case "feb":
+                if (isLeapYear(year)) return 29;
+                else return 28;
+            case "mar": case "may": case "jul": case "aug": case "oct": case "dec": case "jan": return 31;
+            case "jun": case "apr": case "nov": case "sep": return 30;
+        }
+        return -1;
+    }
+
+    //overloaded to take int instead of string month value
+    public static int lastDayOfMonth(int month, int year) {
+        if (intToMonth(month).equals("invalid")) return -1;
+        return lastDayOfMonth(intToMonth(month), year);
+    }
+
+    //get the current date returned as a string from displayCalendar
+    public static String printToday() {
+        LocalDate today = LocalDate.now();
+        return displayCalendar(today.getDayOfMonth(), today.getMonthValue(), today.getYear());
+    }
 
 }
